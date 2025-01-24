d805b2b2a17b9bd29d0f7bff9ec9a45a4c7f1eb1
fresh4 <fresh1736@gmail.com>
2025-01-24 03:52:12 -0600

    Added lore accurate scoring system

--- Scripts/level_manager.gd
+++ Scripts/level_manager.gd
@@ -12,7 +12,11 @@ signal combo_updated(value);
 @onready var world_environment: WorldEnvironment = %WorldEnvironment
 
 var score: int = 0; ## Total effective score
-var throws: int = 0; ## How many times the ball was "thrown" (hit a pin within a time frame)
+var current_throw: int = 0; ## How many times the ball was "thrown" (hit a pin within a time frame)
+var current_frame: int = 0; ## Which frame (out of 10) we're on
+var frame_scores_list: Array[Array] = []; ## A list of arrays, each of max length 2.
+var frame_display_list: Array[Array] = []; ## Similar to frame_scores, but is the text shown on screen.
+
 var pins: Array[Pin] = []; ## All pins on the field on level load
 var knocked_pins: Array[Pin] = []; ## All pins that have been knocked over to avoid count if hit again
 var combo_pins: Array[Pin] = []; ## All the pins hit within a certain time frame of each other
@@ -27,6 +31,11 @@ func _ready() -> void:
 	camera = player.get_node("%Camera");
 	
 	ball.body_entered.connect(_on_ball_hit_pins);
+	
+	# Init frames
+	for i in range(10):
+		frame_scores_list.append([0,0]);
+		frame_display_list.append([0,0]);
 	
 	# Set up score timer data.
 	c_timer.one_shot = true;
@@ -72,12 +81,15 @@ func _on_ball_hit_pins(body: Node3D) -> void:
 	Globals.freeze_frame(0.35, 0.5);
 	camera._camera_shake(0.1, 0.1);
 	
+	# Pin is hit, start a timer. All pins hit within that timeframe count towards
+	# the score of that "throw".
 	if c_timer.is_stopped(): 
 		c_timer.start();
 		timer_started.emit(c_timer.wait_time);
 		await c_timer.timeout;
-		score += len(combo_pins);
+		calculate_score();
 		score_updated.emit(score);
+		c_timer.wait_time = COMBO_TIMER_S;
 		
 		combo_pins = [];
 		await get_tree().create_timer(2).timeout;
@@ -86,9 +98,49 @@ func _on_ball_hit_pins(body: Node3D) -> void:
 	else: 
 		return;
 
+func calculate_score() -> void:
+	var frame_score: int = len(combo_pins); # Number of pins knocked over this throw.
+	score += frame_score; # Counts the TOTAL knocked over pins this frame.
+	current_throw += 1; # Increment the throw; a frame consists of (max) two throws.
+	
+	# Skip to next frame if strike.
+	if current_throw == 1 and score >= 10:
+		update_frames(10, "X");
+		progress_to_next_frame();
+		return;
+	
+	# We hit a spare! Progress to next frame.
+	if current_throw == 2 and score >= 10:
+		update_frames(frame_score, "/");
+		progress_to_next_frame();
+		return;
+		# TODO: Modify score
+	
+	# Progress to next frame if we've made two throws this frame, regardless of final score.
+	if current_throw >= 2: 
+		update_frames(frame_score);
+		progress_to_next_frame();
+		return;
+	
+	# Currently only made one throw. Add score and continue this frame.
+	if current_throw < 2 and score < 10:
+		update_frames(frame_score);
+		return;
+
+func update_frames(value: int, value2: String = str(value)) -> void:
+	frame_scores_list[current_frame][current_throw - 1] = (value);
+	frame_display_list[current_frame][current_throw - 1] = str(value2);
+	print("RAW SCORES: ", frame_scores_list, " DISPLAYED SCORES: ", frame_display_list);
+
+func progress_to_next_frame() -> void:
+		current_frame += 1; 
+		current_throw = 0;
+		score = 0;
+
 func _on_pin_knocked_over(pin: Pin) -> void:
 	if pin not in knocked_pins:
 		knocked_pins.append(pin);
 	if pin not in combo_pins:
 		combo_pins.append(pin);
 		combo_updated.emit(len(combo_pins));
+		c_timer.wait_time += 0.1;
